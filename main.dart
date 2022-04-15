e102ab36033c7fc2eedb7bc9eb84799814937df4
Imran Ali Khan <70568289+MKServices1@users.noreply.github.com>
2022-04-15 11:27:56 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:testing/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(providers: [
+      ChangeNotifierProvider<CheckProvider>(create: (context)=>CheckProvider()),
+
+    ],child: const MaterialApp(home: Abc(),
+    ),);
+  }
+}
+class Abc extends StatefulWidget {
+  const Abc({Key? key}) : super(key: key);
+
+  @override
+  State<Abc> createState() => _AbcState();
+}
+
+class _AbcState extends State<Abc> {
+  @override
+  Widget build(BuildContext context) {
+    bool _isCheck=false;
+    return Scaffold(
+      key: UniqueKey(),
+      body: Center(
+          child: Consumer<CheckProvider>(
+              builder: (context,checkProvider,child)=>Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+
+
+              const Text('USING STATEFUL BUILDER',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24),),
+
+              ///STATEFUL BUILDER HAS LIMITATIONS, WE CAN't USED CHANGED VALUE OUTSIDE THE WIDGET
+              StatefulBuilder(builder: (BuildContext context, StateSetter setState){
+                return
+
+                  CheckboxListTile(
+                    value: _isCheck,
+                    onChanged:(value){setState(() {
+                      _isCheck=value!;
+                      print('VALUE: ');
+                      print(value);
+                      print('IS CHECK: ');
+                      print(_isCheck);
+                    });},
+                  title:Text('Pine Apple '),);
+              }),
+
+                  const Text('USING PROVIDER ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24),),
+                  ...checkProvider.filter.map((isChecked) => CheckboxClass(Checkbox: isChecked, onChanged: (val){
+                    checkProvider.selectFilter(isChecked, val);
+                    print(isChecked);
+                    print(val);
+                  })).toList(),
+
+            ],
+          )),
+
+      ),
+
+    );
+  }
+}
+
+
+
---
+++ provider.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CheckProvider extends ChangeNotifier{
+  bool _selected=false;
+  bool get selecte =>_selected;
+
+
+
+  final List<CheckBoxTitle> _filter=[
+    CheckBoxTitle(title: "Apple", check: false),
+    CheckBoxTitle(title: "Mango", check: false),
+    CheckBoxTitle(title: "Banana", check: false),
+    CheckBoxTitle(title: "Orange", check: false),
+  ];
+
+  void selectFilter(CheckBoxTitle checkBoxState,bool selected){
+    _filter.firstWhere((element) => element.title==checkBoxState.title).check=selected;
+    notifyListeners();
+  }
+
+  List<CheckBoxTitle> get filter=>_filter;
+
+}
+
+
+class CheckboxClass extends StatelessWidget {
+
+  final CheckBoxTitle Checkbox;
+  final void Function(bool)? onChanged;
+  const CheckboxClass({Key? key,required this.Checkbox,required this.onChanged}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CheckboxListTile(
+      value: Checkbox.check,
+      onChanged: (v)=>onChanged!(v!),
+      title:Text(Checkbox.title!),);
+  }
+}
+
+
+class CheckBoxTitle{
+  final String? title;
+  bool? check;
+
+  CheckBoxTitle({required this.title,required this.check,});
+}
+
+
